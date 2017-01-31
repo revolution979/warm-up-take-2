071e10fc0808cecd6ac62df15d1b2c7e2a2d7a48
game110pm <game110pm@605-110-STU20>
2017-01-31 13:12:07 -0700

    can you see this?

---
+++ 1-31 tk2/1-31 tk2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int opinion;
+
+int main() {
+
+	cout << "two blood cells met and fell in love but alas it was all in vein" << endl;
+	cout << "lol" << endl;
+}//can you see me?
+	
